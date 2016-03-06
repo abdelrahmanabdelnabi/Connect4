1da424afb4dc987ecf39a3f10a38dec03fcb12a8
oddcoder <ahmedsoliman0x666@gmail.com>
2016-03-06 18:53:14 +0200

    re-fixing the backend

--- src/backend/Connect4.java
+++ src/backend/Connect4.java
@@ -25,7 +25,7 @@ public class Connect4 {
 	//switch the players color and check for winner
 	public int insertCoin(int here){
 		int i;
-		for (i=5;i>=0;i--)
+		for (i=ROWS-1;i>=0;i--)
 			if(Array[i][here]==0)
 				break;
 		if (Array[i][here]!=0)
@@ -42,41 +42,41 @@ public class Connect4 {
 	public int getPlayer(){
 		return CurrentPlayer;
 	}
-	public int getWinner(){
+	int getWinner(){
 		int i,j;
-		for(i=0;i<6;i++)
-			for(j=0;j<7;j++){
+		for(i=0;i<ROWS;i++)
+			for(j=0;j<COLUMNS;j++){
 				if (Array[i][j]==0)
 					continue;
-				if(j+3<6)
+				if(j+3<COLUMNS)
 					if(Array[i][j] == Array[i][j+1] && 
 					   Array[i][j] == Array[i][j+2] &&
 					   Array[i][j] == Array[i][j+3])
 						return Array[i][j];
-				if(i+3<7)
+				if(i+3<ROWS)
 					if(Array[i][j] == Array[i+1][j] &&
 					   Array[i][j] == Array[i+2][j] &&
 					   Array[i][j] == Array[i+3][j])
 						return Array[i][j];
-				if(i+3<7 &&j+3<6)
+				if(i+3<ROWS &&j+3<COLUMNS)
 					if(Array[i][j] == Array[i+1][j+1] &&
 					   Array[i][j] == Array[i+2][j+2] &&
 					   Array[i][j] == Array[i+3][j+3])
 						return Array[i][j];
-				if(i+3<7 &&j-3>=0)
+				if(i+3<ROWS &&j-3>=0)
 					if(Array[i][j] == Array[i+1][j-1] &&
 					   Array[i][j] == Array[i+2][j-2] &&
 					   Array[i][j] == Array[i+3][j-3])
 						return Array[i][j];
 			}
 		int zeros=0;
-		for(j=0;j<7;j++)
+		for(j=0;j<COLUMNS;j++)
 			if(Array[0][j]==0)
 				zeros++;
 		return zeros ==0? -1 :0;
 	}
-	public Connect4(){
-		Array = new int[6][7]; //defaults to zero
+	Connect4(){
+		Array = new int[ROWS][COLUMNS]; //defaults to zero
 		CurrentPlayer=1;
 		
 	}
