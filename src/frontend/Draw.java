72f9af74d796d9619df29a796060db5cf5b6cf47
abdulrahman <abdulrahman@HP>
2016-03-06 19:03:47 +0200

    fixed the reversed color problem (color disorientation)

--- src/frontend/Draw.java
+++ src/frontend/Draw.java
@@ -1,6 +1,5 @@
 package frontend;
 
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,112 +9,105 @@ import javax.swing.*;
 import backend.Connect4;
 
 @SuppressWarnings("serial")
-public class Draw extends JPanel implements ActionListener{
-	
+public class Draw extends JPanel implements ActionListener {
+
 	public static final int ROWS = 6;
 	public static final int COLUMNS = 7;
 	private static final int HOR_GAP = 50;
 	private static final int VER_GAP = 150;
 	private static final int TILES_GAP = 10;
 	private static final int TILE = 70;
-	private static final int BOARDWIDTH = 2*HOR_GAP + COLUMNS*TILE + (COLUMNS - 1)*TILES_GAP;
-	private static final int BOARDHEIGHT = TILE + ROWS*TILE + (ROWS - 1)*TILES_GAP;
-	
-	Timer timer = new Timer(25, this);
-	
+	private static final int BOARDWIDTH = 2 * HOR_GAP + COLUMNS * TILE + (COLUMNS - 1) * TILES_GAP;
+	private static final int BOARDHEIGHT = TILE + ROWS * TILE + (ROWS - 1) * TILES_GAP;
+
 	Connect4 newgame = new Connect4();
-	
+
 	MA mouseAdapter = new MA();
-	
+
 	int x, y, source;
 	int currentY;
-	
-	public void actionPerformed(ActionEvent event)
-	{
+
+	public void actionPerformed(ActionEvent event) {
 		currentY = currentY + 2;
 		System.out.println(event.getActionCommand());
 		repaint();
 	}
-	
-	public void drawing(int xx, int yy, int source)
-	{
+
+	public void drawing(int xx, int yy, int source) {
 		x = xx;
 		y = yy;
 		this.source = source;
 		repaint();
 	}
-	
-	private void initUI(Graphics g)
-	{
+
+	private void initUI(Graphics g) {
 		super.paintComponent(g);
 		g.setColor(Color.BLUE);
-		g.fillRoundRect(10, VER_GAP - TILE/2, BOARDWIDTH, BOARDHEIGHT, 50, 50);
+		g.fillRoundRect(10, VER_GAP - TILE / 2, BOARDWIDTH, BOARDHEIGHT, 50, 50);
 		int[][] board = newgame.getArray();
-		for(int i = 0; i < 6; i++)
-		{
-			for(int j = 0; j < 7; j++)
-			{
-				int horStart = 10 + HOR_GAP + j*(TILE + TILES_GAP);
-				int verStart = VER_GAP + i*(TILE + TILES_GAP);
-				
-				if(board[i][j] == 0)
+		for (int i = 0; i < ROWS; i++) {
+			for (int j = 0; j < COLUMNS; j++) {
+				int horStart = 10 + HOR_GAP + j * (TILE + TILES_GAP);
+				int verStart = VER_GAP + i * (TILE + TILES_GAP);
+
+				if (board[i][j] == 0)
 					g.setColor(getBackground());
-				else if(board[i][j] == 1)
-					g.setColor(Color.YELLOW);
-				else if(board[i][j] == 2)
+				else if (board[i][j] == 1)
 					g.setColor(Color.RED);
+				else if (board[i][j] == 2)
+					g.setColor(Color.YELLOW);
 				g.fillOval(horStart, verStart, TILE, TILE);
 			}
 		}
 	}
-	
-	public void paintComponent(Graphics g)
-	{
+
+	public void paintComponent(Graphics g) {
 		initUI(g);
-		
+
 		g.setColor(newgame.getPlayer() == 1 ? Color.RED : Color.YELLOW);
-		//calculate above which column will the coin be displayed
-		int column = (x - HOR_GAP - 10)/(TILE + TILES_GAP);
-		if(column < 7)
+		// calculate above which column will the coin be displayed
+		int column = (x - HOR_GAP - 10) / (TILE + TILES_GAP);
+		if (column < 7)
 			g.fillOval(calcXCoord(column), 40, TILE, TILE);
-		
-		if(this.source == 1)
-		{
-			if(column >= 7)
-				JOptionPane.showMessageDialog(this, "cannot insert coin here", "error", JOptionPane.PLAIN_MESSAGE);
-			if(newgame.insertCoin(column) == -2)
-				JOptionPane.showMessageDialog(this, "cannot insert coin here", "error", JOptionPane.PLAIN_MESSAGE);
-			else
-			{
+
+		if (this.source == 1) {
+			if (column >= 7)
+				JOptionPane.showMessageDialog(this, "cannot insert coin here",
+						"error", JOptionPane.PLAIN_MESSAGE);
+			else if (newgame.insertCoin(column) == -2)
+				JOptionPane.showMessageDialog(this, "cannot insert coin here",
+						"error", JOptionPane.PLAIN_MESSAGE);
+			else {
 				initUI(g);
 				g.setColor(newgame.getPlayer() == 1 ? Color.RED : Color.YELLOW);
-				if(column < 7)
+				if (column < 7)
 					g.fillOval(calcXCoord(column), 40, TILE, TILE);
 				int winner = newgame.getWinner();
-				if(winner == 1 )
-					JOptionPane.showMessageDialog(this, "Player 1 wins\nCongratulations", "error", JOptionPane.PLAIN_MESSAGE);
-				else if(winner == 2)
-					JOptionPane.showMessageDialog(this, "Player 2 wins\nCongratulations", "error", JOptionPane.PLAIN_MESSAGE);
-				else if(winner == -1) //draw
-					JOptionPane.showMessageDialog(this, "The game ended as a DRAW", "error", JOptionPane.PLAIN_MESSAGE);
+				if (winner == 1)
+					JOptionPane.showMessageDialog(this,
+							"Player 1 wins\nCongratulations", "error",
+							JOptionPane.PLAIN_MESSAGE);
+				else if (winner == 2)
+					JOptionPane.showMessageDialog(this,
+							"Player 2 wins\nCongratulations", "error",
+							JOptionPane.PLAIN_MESSAGE);
+				else if (winner == -1) // draw
+					JOptionPane.showMessageDialog(this,
+							"The game ended as a DRAW", "error",
+							JOptionPane.PLAIN_MESSAGE);
 			}
-			
+
 		}
 	}
-//	private void translate(Graphics g)
-//	{
-//		g.setColor(true ? Color.RED : Color.YELLOW);
-//		g.fillOval(calcXCoord(5),currentY , TILE, TILE);
-//		timer.start();
-//	}
-	
-	private int calcXCoord(int columnNum)
-	{
-		return 10 + HOR_GAP + columnNum*(TILE + TILES_GAP);
-	}
-	
-	private int calcYCoord(int rowNum)
-	{
-		return VER_GAP + rowNum*(TILE + TILES_GAP);
+
+//	 private void translate(Graphics g)
+//	 {
+//	 g.setColor(true ? Color.RED : Color.YELLOW);
+//	 g.fillOval(calcXCoord(5),currentY , TILE, TILE);
+//	 timer.start();
+//	 }
+
+	private int calcXCoord(int columnNum) {
+		return 10 + HOR_GAP + columnNum * (TILE + TILES_GAP);
 	}
 }
--- src/frontend/Shapes.java
+++ src/frontend/Shapes.java
@@ -1,23 +1,19 @@
 package frontend;
 
-
-import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.*;
 
-import backend.Connect4;
-
 public class Shapes {
 	int x, y;
 	public static Draw obj = new Draw();
 
-public static void main(String[] args) {
-		
+	public static void main(String[] args) {
+
 		JFrame frame = new JFrame();
 		frame.setVisible(true);
-		frame.setSize(1024,720);
+		frame.setSize(1024, 720);
 		frame.setTitle("connect 4 test");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JPanel panel = new JPanel();
@@ -26,46 +22,42 @@ public static void main(String[] args) {
 		frame.add(obj);
 		obj.addMouseListener(new MA());
 		obj.addMouseMotionListener(new MA());
-	
+
 	}
 
-static class MA extends MouseAdapter {
+	static class MA extends MouseAdapter {
 
-	static int pointerX, pointerY, source;
-	
+		static int pointerX, pointerY, source;
 
-	public void mouseClicked(MouseEvent event) {
-		pointerX = event.getX();
-		pointerY = event.getY();
-		source = 1;
-		obj.drawing(pointerX, pointerY, source);
-		
-	}
+		public void mouseClicked(MouseEvent event) {
+			pointerX = event.getX();
+			pointerY = event.getY();
+			source = 1;
+			obj.drawing(pointerX, pointerY, source);
 
-	public void mouseMoved(MouseEvent event) {
-		pointerX = event.getX();
-		pointerY = event.getX();
-		source = 2;
-		obj.drawing(pointerX, pointerY, source);
-	}
+		}
 
-	public void mouseDragged(MouseEvent event) {
-		pointerX = event.getX();
-		pointerY = event.getY();
-		source = 3;
-		obj.drawing(pointerX, pointerY, source);
+		public void mouseMoved(MouseEvent event) {
+			pointerX = event.getX();
+			pointerY = event.getX();
+			source = 2;
+			obj.drawing(pointerX, pointerY, source);
+		}
+
+		public void mouseDragged(MouseEvent event) {
+			pointerX = event.getX();
+			pointerY = event.getY();
+			source = 3;
+			obj.drawing(pointerX, pointerY, source);
+		}
+
+		public int getPointerX() {
+			return pointerX;
+		}
+
+		public int getPointerY() {
+			return pointerY;
+		}
 	}
-	
-	public int getPointerX()
-	{
-		return pointerX;
-	}
-	
-	public int getPointerY()
-	{
-		return pointerY;
-	}
-}
 
 }
-
